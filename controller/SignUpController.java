dddc67dbff33b6d33e5b8cf23cc22e2b3c7b487b
kseveur <kseveur@hotmail.com>
2018-11-27 12:00:21 -0500

    Update SignUpController.java

--- controller/SignUpController.java
+++ controller/SignUpController.java
@@ -2,34 +2,39 @@ package controller;
 
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.ResourceBundle;
-
-
-import javafx.event.ActionEvent;
+import java.util.concurrent.TimeUnit;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class SignUpController implements Initializable{
+public class SignUpController extends RMIClientInitializer implements Initializable{
 	
 	//Views
 	@FXML private Text errorText;
 	@FXML private TextField firstNameSUField;
 	@FXML private TextField lastNameSUField;
-	@FXML private TextField emailSUField;
+	@FXML private TextField userNameField;
 	@FXML private TextField passwordSUField;
 	@FXML private Button signUpButton;
-
+	public int userSessionId = 0;
+	
+	@SuppressWarnings("unused")
 	private Main main;
+	@SuppressWarnings("unused")
 	private Stage primaryStage;
 
 	
@@ -39,11 +44,43 @@ public class SignUpController implements Initializable{
 	
 	
 	
-	public void signUp() {
+	public void signUp() throws MalformedURLException, RemoteException, NotBoundException { //rmi method
+		
+		boolean firstAllLetters = ((CharSequence) firstNameSUField).chars().allMatch(Character::isLetter);
+		boolean lastAllLetters = ((CharSequence) lastNameSUField).chars().allMatch(Character::isLetter);
+		if(!firstAllLetters || !lastAllLetters) {
+			alertAlphasOnly();
+			return;
+		}
+		
+		/////////////////////////********* RMI CODE START *********////////////////////////
+		
+	
 		System.out.println("FirstName: " + firstNameSUField.getText());
 		System.out.println("LastName: " + lastNameSUField.getText());
-		System.out.println("Email: " + emailSUField.getText());
+		System.out.println("Email: " + userNameField.getText());
 		System.out.println("Password: " + passwordSUField.getText());
+		
+		RMILoader();
+		try {
+		adminServerinterface client = (adminServerinterface) Naming.lookup("rmi://"+ getAdminIp() +"/binded"); //connecting to RMI Server
+		System.out.println("Successfully Connected to Admin module's RMI Server");	//if connection successful 
+		if (client.registerUser(firstNameSUField.getText(), lastNameSUField.getText(),userNameField.getText(), "rmitest5", passwordSUField.getText())) {
+			System.out.println("User Registered Succesfully");
+			//load sign in page 
+			
+			TimeUnit.SECONDS.sleep(2);
+			searchScreenLoader();
+		}
+		else {
+			showAlert2();  //username not available 
+		}
+		} catch (Exception e) {
+		 System.out.println ("Could not Connect to RMI Server. Make sure rmi registry is running on server machine and Server IP is correct. ");
+		 System.out.println(e);
+			
+		}
+			////////////////////////********* RMI CODE END *********////////////////////////
 	}
 	
 	public void showAlert2() {
@@ -55,10 +92,13 @@ public class SignUpController implements Initializable{
 	}
 	
 
-	/*public void searchViewSwitch() {
-		Stage stage = (Stage) signUpButton.getScene().getWindow();
-	    stage.close();
-	}*/
+	public void alertAlphasOnly() {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Error");
+		alert.setHeaderText("Incorrect name format!");
+		alert.setContentText("Please enter only letters in the first name and last name fields!");
+		alert.showAndWait();		
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -71,7 +111,8 @@ public class SignUpController implements Initializable{
 	
 	//This method is only an event listener for testing and will be changed to a remove the action event.
 	//Method will be called upon complete sign-in
-	public void handleButtonAction(ActionEvent event) {
+	//public void handleButtonAction(ActionEvent event) 
+	public void searchScreenLoader() {
 		Stage stage2 = (Stage) signUpButton.getScene().getWindow();
 	    stage2.close();
 		try {
